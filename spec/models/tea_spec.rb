fb1dc89e07ed91e342a098a9bfdd5b4f3ea4717b
ChloeLL <chloell@protonmail.ch>
2022-03-02 04:09:51 -0700

    Modify model tests

--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe Tea, type: :model do
   end
 
   describe 'validations' do
-    it { should validate_presence_of(:title, :description, :temperature, :brew_time) }
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :temperature }
+    it { should validate_presence_of :brew_time }
   end
 end
